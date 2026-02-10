13f7d9007c315dee4e5d4549af57677d2e917cb1
Sautrik <sr9973@srmist.edu.in>
2026-02-10 11:19:29 +0530

    Initial OOPS Java programs

---
+++ Banner.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Banner {
+    public static void main(String args[]) {
+
+        System.out.println("   *      \t   *      \t  *      \t  **");
+        System.out.println("  * *     \t  * *     \t *  *     \t **");
+        System.out.println(" *   *    \t *   *    \t *  *     \t **");
+        System.out.println(" *   *    \t *   *    \t *  *     \t **");
+        System.out.println(" *   *    \t *   *    \t  **      \t   *");
+        System.out.println(" *   *    \t *   *    \t  *       \t    *");
+        System.out.println(" *   *    \t *   *    \t  *       \t     *");
+        System.out.println("  * *     \t  * *     \t  *       \t     *");
+        System.out.println("   *      \t   *      \t  *       \t  *");
+    }
+}
